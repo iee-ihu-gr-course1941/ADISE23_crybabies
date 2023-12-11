e89f3bea1c03ce6ab5901788b685cd696cddd216
eleniDav <elenidav19@yahoo.com>
2023-12-11 14:54:03 +0000

    attempting to draw the board

--- www/js/myludo.js
+++ www/js/myludo.js
@@ -8,8 +8,27 @@ function draw_empty_board() {
         t+='</tr>';
     }
     t += '</table>';
-    document.getElementById("ludo_board").innerHTML += t;
+    $('#ludo_board').html(t);
+}
+$(draw_empty_board);
+
+/*
+function fill_board() {
+    $.ajax(
+            {url: "ludo.php/board/",
+            success: fill_board_by_data
+            }
+        );
 }
 
-
-    
+function fill_board_by_data(data) {
+    for(var i=0;i<data.length;i++) {
+        var o = data[i];
+        var id = '#square_'+ o.x +'_' + o.y;
+        //var c = (o.piece!=null)?o.piece_color + o.piece:'';
+        //var im = c;
+        //var im = (o.piece!=null)?'<img class="piece" src="images/'+c+'.png">':'';
+        //$(id).addClass(o.b_color+'_square').html(im);
+    }
+}
+    */
